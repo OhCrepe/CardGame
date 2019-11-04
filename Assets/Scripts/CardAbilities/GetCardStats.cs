8ae3ac10ebe55681de1c537d162181a894de66e7
Tyler-PC\Tyler <tylerthorn@hotmail.co.uk>
2019-11-04 09:49:09 +0000

    Unit cards now display strength and health

--- Assets/Scripts/CardAbilities/GetCardStats.cs
+++ Assets/Scripts/CardAbilities/GetCardStats.cs
@@ -5,19 +5,34 @@ using UnityEngine.UI;
 
 public class GetCardStats : MonoBehaviour {
 
+	private CardData data; // This cards CardData script
+
 	/*
 	*	Triggers on start
 	*/
 	void Start () {
+		data = GetComponent<CardData>();
 		GetAndSetCost();
+		if(data.cardType == CardData.Type.MINION){
+			GetAndSetStrength();
+			GetAndSetHealth();
+		}
 	}
 
 	/*
-	* Works out the cost to activate this card, and updates the gold cost text to match
+	* Works out the stats of this card, and updates the card text to match
 	*/
 	private void GetAndSetCost(){
-		int cost = transform.gameObject.GetComponent<CardData>().cost;
+		int cost = data.cost;
 		transform.Find("Coin Counter/Text").GetComponent<Text>().text = "" + cost;
+	}
+	private void GetAndSetStrength(){
+		int strength = data.strength;
+		transform.Find("Strength/Text").GetComponent<Text>().text = "" + strength;
+	}
+	private void GetAndSetHealth(){
+		int health = data.health;
+		transform.Find("Health/Text").GetComponent<Text>().text = "" + health;
 	}
 
 }
