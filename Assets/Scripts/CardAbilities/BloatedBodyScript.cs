2726edaddb9204b41d4c6d8fdabea1d13891ff7e
Thorn <tt16786@essex.ac.uk>
2019-11-08 11:03:54 +0000

    Implemented the 'Bloated Body' Card

---
+++ Assets/Scripts/CardAbilities/BloatedBodyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloatedBodyScript : CardAbility
+{
+
+  public int damageDealt;
+
+  /*
+  *	Ability that triggers on death.
+  *	Gains 6 gold. It doesn't matter how to unit dies in the case, so we override OnKillAbility()
+  */
+  public override void OnKillAbility(bool combat){
+    GameObject[] cards = GameObject.FindGameObjectsWithTag("Card");
+    foreach(GameObject card in cards){
+      if(card.GetComponent<CardData>().cardType == CardData.Type.MINION){
+        if(card.transform.parent.GetComponent<Dropzone>().zoneType == Dropzone.Zone.FIELD){
+          card.GetComponent<CardData>().DealDamage(damageDealt, false);
+        }
+      }
+    }
+  }
+
+}
